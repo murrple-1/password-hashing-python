99f93a317681a6dba0adf8668f18e68aec9654ce
Murray Christopherson <murraychristopherson@gmail.com>
2016-10-26 22:12:55 -0400

    Adding a CLI implementation

---
+++ generatehash.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+import passwordhash
+
+def main():
+    args = sys.argv
+    if len(args) < 2:
+        print 'Usage: {0} [PASSWORD]'.format(args[0])
+        sys.exit(1)
+
+    print passwordhash.createHash(args[1])
+
+if __name__ == "__main__":
+    main()
