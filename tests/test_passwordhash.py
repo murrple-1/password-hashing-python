348ab7524d365874fe447b7ce490e4cb5560173e
Murray Christopherson <murraychristopherson@gmail.com>
2018-01-26 11:50:54 -0500

    Preparing for PyPI release

--- tests/test_passwordhash.py
+++ tests/test_passwordhash.py
@@ -1,31 +1,31 @@
 import unittest
 
-import passwordhash
+import password_hashing
 
 
 class TestPasswordHash(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.hash_of_password = passwordhash.create_hash('password')
+        cls.hash_of_password = password_hashing.create_hash('password')
 
     def test_create_hash(self):
-        passwordhash.create_hash('test_password')
+        password_hashing.create_hash('test_password')
 
     def test_successful_validation(self):
         self.assertTrue(
-            passwordhash.validate_password(
+            password_hashing.validate_password(
                 'password',
                 TestPasswordHash.hash_of_password))
 
     def test_unsuccessful_validation(self):
         self.assertFalse(
-            passwordhash.validate_password(
+            password_hashing.validate_password(
                 'wrong_password',
                 TestPasswordHash.hash_of_password))
 
     def test_type_error(self):
         with self.assertRaises(TypeError):
-            passwordhash.validate_password('password', algorithm='')
+            password_hashing.validate_password('password', algorithm='')
 
 
 if __name__ == '__main__':
