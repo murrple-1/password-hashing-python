6eb56e1ff0656d65b036afeb5da4cb33a0641f44
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-03 16:24:02 -0400

    Improving the test suite by checking that string-type doesn't matter

--- tests/test_passwordhash.py
+++ tests/test_passwordhash.py
@@ -6,26 +6,62 @@ import password_hashing
 class TestPasswordHash(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.hash_of_password = password_hashing.create_hash('password')
+        cls.hash_of_b_password = password_hashing.create_hash(b'b_password')
+        cls.hash_of_u_password = password_hashing.create_hash(u'u_password')
 
-    def test_create_hash(self):
-        password_hashing.create_hash('test_password')
+    def test_create_hash_b(self):
+        password_hashing.create_hash(b'test_password')
 
-    def test_successful_validation(self):
+    def test_create_hash_u(self):
+        password_hashing.create_hash(u'test_password')
+
+    def test_successful_validation_b2b(self):
         self.assertTrue(
             password_hashing.validate_password(
-                'password',
-                TestPasswordHash.hash_of_password))
+                b'b_password',
+                TestPasswordHash.hash_of_b_password))
 
-    def test_unsuccessful_validation(self):
+    def test_successful_validation_u2u(self):
+        self.assertTrue(
+            password_hashing.validate_password(
+                u'u_password',
+                TestPasswordHash.hash_of_u_password))
+
+    def test_successful_validation_b2u(self):
+        self.assertTrue(
+            password_hashing.validate_password(
+                b'u_password',
+                TestPasswordHash.hash_of_u_password))
+
+    def test_successful_validation_u2b(self):
+        self.assertTrue(
+            password_hashing.validate_password(
+                u'b_password',
+                TestPasswordHash.hash_of_b_password))
+
+    def test_unsuccessful_validation_b2b(self):
         self.assertFalse(
             password_hashing.validate_password(
-                'wrong_password',
-                TestPasswordHash.hash_of_password))
+                b'wrong_password',
+                TestPasswordHash.hash_of_b_password))
 
-    def test_type_error(self):
-        with self.assertRaises(TypeError):
-            password_hashing.validate_password('password', algorithm='')
+    def test_unsuccessful_validation_u2u(self):
+        self.assertFalse(
+            password_hashing.validate_password(
+                u'wrong_password',
+                TestPasswordHash.hash_of_u_password))
+
+    def test_unsuccessful_validation_b2u(self):
+        self.assertFalse(
+            password_hashing.validate_password(
+                b'wrong_password',
+                TestPasswordHash.hash_of_u_password))
+
+    def test_unsuccessful_validation_u2b(self):
+        self.assertFalse(
+            password_hashing.validate_password(
+                u'wrong_password',
+                TestPasswordHash.hash_of_b_password))
 
 
 if __name__ == '__main__':
