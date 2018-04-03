8417a24be292204821b4e5bcb29e51581904fe34
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-03 16:43:22 -0400

    Add element of randomness to test cases, to ensure it works 'in general'

--- tests/test_passwordhash.py
+++ tests/test_passwordhash.py
@@ -1,66 +1,82 @@
 import unittest
+import random
+import string
 
 import password_hashing
 
 
 class TestPasswordHash(unittest.TestCase):
+    @staticmethod
+    def generate_b_str(size=12):
+        available_chars = string.printable.encode('utf-8')
+        return b''.join(random.choice(available_chars) for _ in range(size))
+
+    @staticmethod
+    def generate_u_str(size=12):
+        available_chars = string.printable
+        return u''.join(random.choice(available_chars) for _ in range(size))
+
     @classmethod
     def setUpClass(cls):
-        cls.hash_of_b_password = password_hashing.create_hash(b'b_password')
-        cls.hash_of_u_password = password_hashing.create_hash(u'u_password')
+        cls.b_password = TestPasswordHash.generate_b_str()
+        cls.u_password = TestPasswordHash.generate_u_str()
+        cls.u_b_password = cls.b_password.decode('utf-8')
+        cls.b_u_password = cls.u_password.encode('utf-8')
+        cls.hash_of_b_password = password_hashing.create_hash(cls.b_password)
+        cls.hash_of_u_password = password_hashing.create_hash(cls.u_password)
 
     def test_create_hash_b(self):
-        password_hashing.create_hash(b'test_password')
+        password_hashing.create_hash(TestPasswordHash.generate_b_str())
 
     def test_create_hash_u(self):
-        password_hashing.create_hash(u'test_password')
+        password_hashing.create_hash(TestPasswordHash.generate_u_str())
 
     def test_successful_validation_b2b(self):
         self.assertTrue(
             password_hashing.validate_password(
-                b'b_password',
+                TestPasswordHash.b_password,
                 TestPasswordHash.hash_of_b_password))
 
     def test_successful_validation_u2u(self):
         self.assertTrue(
             password_hashing.validate_password(
-                u'u_password',
+                TestPasswordHash.u_password,
                 TestPasswordHash.hash_of_u_password))
 
     def test_successful_validation_b2u(self):
         self.assertTrue(
             password_hashing.validate_password(
-                b'u_password',
+                TestPasswordHash.b_u_password,
                 TestPasswordHash.hash_of_u_password))
 
     def test_successful_validation_u2b(self):
         self.assertTrue(
             password_hashing.validate_password(
-                u'b_password',
+                TestPasswordHash.u_b_password,
                 TestPasswordHash.hash_of_b_password))
 
     def test_unsuccessful_validation_b2b(self):
         self.assertFalse(
             password_hashing.validate_password(
-                b'wrong_password',
+                TestPasswordHash.generate_b_str(),
                 TestPasswordHash.hash_of_b_password))
 
     def test_unsuccessful_validation_u2u(self):
         self.assertFalse(
             password_hashing.validate_password(
-                u'wrong_password',
+                TestPasswordHash.generate_u_str(),
                 TestPasswordHash.hash_of_u_password))
 
     def test_unsuccessful_validation_b2u(self):
         self.assertFalse(
             password_hashing.validate_password(
-                b'wrong_password',
+                TestPasswordHash.generate_b_str(),
                 TestPasswordHash.hash_of_u_password))
 
     def test_unsuccessful_validation_u2b(self):
         self.assertFalse(
             password_hashing.validate_password(
-                u'wrong_password',
+                TestPasswordHash.generate_u_str(),
                 TestPasswordHash.hash_of_b_password))
 
 
